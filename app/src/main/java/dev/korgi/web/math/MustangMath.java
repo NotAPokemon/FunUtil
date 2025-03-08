d73133a2a0caa12a335c14c773b789cf2add1b63
NotAPokemon <92272566+NotAPokemon@users.noreply.github.com>
2025-03-08 12:59:10 -0800

    complie

--- app/src/main/java/dev/korgi/web/math/MustangMath.java
+++ app/src/main/java/dev/korgi/web/math/MustangMath.java
@@ -1,6 +1,10 @@
 package dev.korgi.web.math;
 
 public class MustangMath {
+    private Function function;
+    private RationalFunction rFunction;
+    private ImaginaryNumber i;
+
     public static double RotationsToDegrees(double rotations, double gearRatio){
         return (rotations * gearRatio) / 360.0;
     }
